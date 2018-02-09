5db9177ddf2c8ec090eefa23d5f3af34d395d2cd
margb13 <marg.b13@hotmail.com>
2018-02-09 11:09:17 -0500

    Añadiendo estructura login y conexión de firebase

---
+++ js/connect.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCoW5ShLp1__j0KQWXrPDxgr2dnpvL4_dw',
+  authDomain: 'insta-collage-3b1ba.firebaseapp.com',
+  databaseURL: 'https://insta-collage-3b1ba.firebaseio.com',
+  projectId: 'insta-collage-3b1ba',
+  storageBucket: 'insta-collage-3b1ba.appspot.com',
+  messagingSenderId: '730634905049'
+};
+firebase.initializeApp(config);
