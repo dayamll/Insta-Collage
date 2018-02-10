ab3913ab97b67e287c7612566eb6f8779c8cebc1
margb13 <marg.b13@hotmail.com>
2018-02-10 11:19:46 -0500

    Añadiendo login con fb

--- js/login.js
+++ js/login.js
@@ -56,7 +56,8 @@ window.addEventListener('load', function() {
   // Iniciando autentificación con Google
   var providerGoogle = new firebase.auth.GoogleAuthProvider();
 
-  googleLogin.addEventListener('click', function() {
+  googleLogin.addEventListener('click', function(event) {
+    event.preventDefault();
     firebase.auth().signInWithPopup(providerGoogle).then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
@@ -70,8 +71,39 @@ window.addEventListener('load', function() {
         profilePhoto: user.photoURL
       }).then(
         user => {
-          $(location).attr('href', 'collage.html');
+          window.location.href = 'collage.html';
         });
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  });
+
+  // login con Facebook
+  var providerFb = new firebase.auth.FacebookAuthProvider();
+  facebookLogin.addEventListener('click', function(event) {
+    event.preventDefault();
+    firebase.auth().signInWithPopup(providerFb).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      firebase.database().ref('users/' + user.uid).set({
+        name: user.displayName,
+        email: user.email,
+        profilePhoto: user.photoURL,
+      }).then(user => {
+        window.location.href = 'collage.html';
+      });
+      console.log(user);
+      // $(location).attr('href', 'home.html');
       // ...
     }).catch(function(error) {
       // Handle Errors here.
